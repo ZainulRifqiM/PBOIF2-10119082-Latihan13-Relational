76a960d409da744f5165400b1a76e9b92cb198d1
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-10-17 17:07:09 +0700

    PBOIF2-10119082-Latihan13-Relational

---
+++ src/pboif2/pkg10119082/latihan13/relational/PBOIF210119082Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan13.relational;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi Program : Relational
+ */
+public class PBOIF210119082Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil2 >= bil1));
+        System.out.println("a <= b = " + (bil2 <= bil1));
+    }
+    
+}
